357bf46c09583710c3a291e4bdef60cda4becf59
valter goes <valtergoes@outlook.com>
2023-09-27 13:50:33 -0300

    feat: Adicionado tema claro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate} from 'react-router-dom';
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Button>Home</Button>} />
+            <Route path="/" element={<Button variant='contained' color='primary'>Home</Button>} />
 
     {/* redireciona para a rota /Home caso o usuário digite uma rota inválida */}
             <Route path="*" element={<Navigate to="/"/>} />
