5b421df1811af5d3218f86274b7db910f3d5797d
valter goes <valtergoes@outlook.com>
2023-09-28 14:56:37 -0300

    feat: Ajustando a responsividade do layout base

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,7 +6,7 @@ export const Home = () => {
 
     return (
 
-        <LayoutBaseDePagina titulo='Home'>
+        <LayoutBaseDePagina titulo='Home' barraDeFerramentas={<>Barra de Ferramentas</>}>
             <h1>CheckList de filmes</h1>
         </LayoutBaseDePagina>
 
--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import { Icon, IconButton, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 import { useDrawerContext } from '../contexts';
@@ -5,11 +6,13 @@ import { useDrawerContext } from '../contexts';
 interface ILayoutBaseDePaginaProps {
     children: React.ReactNode;
     titulo: string;
+    barraDeFerramentas?: ReactNode;
 }
 
-export  const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({children, titulo}) => {
+export  const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({children, titulo, barraDeFerramentas}) => {
     // configura o breakpoint para o tamanho do dispositivo
     const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+    const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
     // configura o tema
     const theme = useTheme();
     // obtém a função de abrir/fechar o drawer
@@ -19,7 +22,7 @@ export  const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({childre
     return (
         <Box height="100%" display="flex" flexDirection="column" gap={1}>
 
-            <Box display="flex" alignItems="center" height={theme.spacing(12)} padding={1} gap={5}>
+            <Box display="flex" alignItems="center" padding={1} gap={5} height={theme.spacing(smDown ? 6 : mdDown ?  8 : 12)}  >
 
                 {/* // se o dispositivo for menor que o breakpoint, exibe o botão de menu */}
                 {smDown && (
@@ -28,16 +31,24 @@ export  const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({childre
                     </IconButton>
                 )}
 
-                <Typography variant={'h5'}>
+                <Typography 
+                    whiteSpace="nowrap"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                    variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+                >
                     {titulo}
                 </Typography>
+
             </Box>
 
-            <Box>
-                Barra de ferramentas
-            </Box>
+            {barraDeFerramentas && (
+                <Box>
+                    {barraDeFerramentas}
+                </Box>
+            )}
 
-            <Box>
+            <Box flex={1} overflow="auto">
                 {children}
             </Box>
 
