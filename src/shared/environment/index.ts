e568dda658a7a5ab927d34b1f2ea59f269d626ca
valter goes <valtergoes@outlook.com>
2023-10-06 09:41:10 -0300

    feat: Configurado as variáveis de ambiente

---
+++ src/shared/environment/index.ts
@@ -0,0 +1,25 @@
+
+
+
+export const Environment = {
+
+    /**
+     * Definie o limite de filmes por página
+     */
+    LIMITE_DE_FILMES_POR_PAGINA: 10,
+
+    /**
+     * Define o valor do campo de busca 
+     */
+    INPUT_DE_BUSCA: 'Pesquisar por ...',
+
+    /**
+     * Define a mensagem de erro para o input de busca
+     */
+    LISTAGEM_VAZIA: 'Nenhum filme encontrado',
+
+    /**
+     * Url base de consulta dos dados da API
+     */
+    URL_BASE: 'http://localhost:3333',
+};
--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
+
 import { responseInterceptor } from './interceptors/ResponseInterceptor';
 import { errorInterceptor } from './interceptors';
+import { Environment } from '../../../environment';
 
 
-// Create a instance of axios to use the same base url.
 const Api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: Environment.URL_BASE,
 });
 
 
 Api.interceptors.response.use(
+    
     (response) => responseInterceptor(response),
     (error) => errorInterceptor(error),
 );
